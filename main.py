991ba5c85ed5ba2c9971373d6aeb7448db62446b
Sachith <110117965+sacheex@users.noreply.github.com>
2023-06-16 12:02:08 -0700

    Creates main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv() # load environment variables from a .env file
+
+parser = argparse.ArgumentParser(description="Audio transcriber")
+
+parser.add_argument("-a", "--audio", type=str, help="Path to audio file")
+
+args = parser.parse_args()
+
+# Transcribe audio file
+def transcribe(audio_file):
+    # openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = "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"
+
+    audio = open(audio_file, 'rb') #open audio file with given path
+
+    transcript = openai.Audio.transcribe("whisper-1", audio)
+
+    return transcript.text
+
+# print result
+if args.audio:
+    audio = args.audio
+    result = transcribe(audio)
+    print(result)
+
+else:
+    print("Invalid Audio path")
